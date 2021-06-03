592d3e229ecc7bf1920a259c804e56b280c15fc2
Lichuan Shang <lichuan.shang@gmail.com>
2021-06-03 15:59:36 +0800

    fix: use args, then env

--- src/args.rs
+++ src/args.rs
@@ -1,8 +1,9 @@
+use std::env;
 use structopt::StructOpt;
 
-/// An elasticsearch snapshots management tool
+/// Elasticsearch index cleaner
 #[derive(StructOpt, Debug)]
-#[structopt(name = "elasticsearch snapshot")]
+#[structopt(name = "elasticsearch index cleaner")]
 pub struct Opt {
     // A flag, true if used in the command line. Note doc comment will
     // be used for the help message of the flag. The name of the
@@ -16,26 +17,50 @@ pub struct Opt {
     #[structopt(short, long, parse(from_occurrences))]
     pub verbose: u8,
 
-    /// Elasticsearch address
-    #[structopt(short = "h", long, default_value = "http://localhost:9200")]
-    pub elasticsearch_addr: String,
+    /// Elasticsearch address, or use ELASTICSEARCH_ADDR env
+    #[structopt(short = "h", long)]
+    pub elasticsearch_addr: Option<String>,
 
-    /// Elasticsearch repository
+    /// Elasticsearch repository, or use ELASTICSEARCH_REPO env
     #[structopt(short = "r", long)]
-    pub elasticsearch_repo: String,
+    pub elasticsearch_repo: Option<String>,
 
-    /// Elasticsearch indices to filter, comma separated list of index prefix,
-    /// i.e app-,logstash-
-    #[structopt(
-        short = "f",
-        long,
-        default_value = "app-*,istio-system-*,kong-*,kube-system-*,pulsar-*,\
-                         logstash-*,haproxy-*,nginx-*,eksfan-logstash-*,\
-                         kong-logstash-*,kongingress-*,app-meican-logstash-*"
-    )]
-    pub index_filter: String,
+    /// Elasticsearch indices to filter, comma separated list of index prefix
+    ///
+    /// i.e:
+    ///  "istio-system-*,kong-*,kube-system-*,pulsar-*,logstash-*
+    #[structopt(short = "f", long)]
+    pub index_filter: Option<String>,
 
-    /// Elasticsearch days to keep
+    /// How many days to keep the indices
     #[structopt(short = "k", long, default_value = "15")]
     pub keep_days: u32,
+}
+
+pub fn value_or_env(
+    key: &str, other: Option<String>,
+) -> anyhow::Result<String> {
+    match other {
+        Some(v) => Ok(v),
+        None => match env::var(key) {
+            Ok(v) => Ok(v),
+            Err(e) => {
+                let context = format!("{} must be set", key);
+                Err(anyhow::Error::new(e).context(context))
+            }
+        },
+    }
+}
+
+pub fn env_or(key: &str, other: Option<String>) -> anyhow::Result<String> {
+    match env::var(key) {
+        Ok(v) => Ok(v),
+        Err(e) => match other {
+            Some(v) => Ok(v),
+            None => {
+                let context = format!("{} must be set", key);
+                Err(anyhow::Error::new(e).context(context))
+            }
+        },
+    }
 }
